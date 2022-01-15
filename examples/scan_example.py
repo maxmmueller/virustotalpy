135203eace0ecc18710894b2e28bbef0ff28fbc9
maxmmueller <66063519+maxmmueller@users.noreply.github.com>
2022-01-15 21:15:26 +0100

    Update scan_example.py

--- examples/scan_example.py
+++ examples/scan_example.py
@@ -2,12 +2,12 @@ from virustotalpy import Virustotal
 
 # scan a file for malware
 scanner = Virustotal("API_KEY")
-resp = scanner.api_request('post', path='C://Users//max20//Pictures//Hintergrund//rainbow mirror.jpeg')
-resp = scanner.api_request('get', path='C://Users//max20//Pictures//Hintergrund//rainbow mirror.jpeg')
+resp = scanner.api_request('post', path='PATH')
+resp = scanner.api_request('get', path='PATH')
 print(resp)
 
 # scan a domain for malware
 scanner = Virustotal("API_KEY")
-resp = scanner.api_request("post", url="https://en.wikipedia.org/wiki/Spider-Man_in_film")
-resp = scanner.api_request("get", url="https://en.wikipedia.org/wiki/Spider-Man_in_film")
+resp = scanner.api_request("post", url="URL")
+resp = scanner.api_request("get", url="URL")
 print(resp)
